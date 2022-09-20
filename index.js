f021f9f54b064426aeba34176239954b4c38dc52
Stephanie Hill <stephni.hill@gmail.com>
2022-09-20 00:21:31 -0700

    app.js

--- index.js
+++ index.js
@@ -1,24 +1,2 @@
 'use strict';
 
-const express = require('express');
-const notFound = require('./err-handlers/404');
-const errorHandler = require('./err-handlers/500')
-const PORT = process.env.PORT || 3002
-
-console.log('helloooo')
-
-const app = express();
-
-app.get('/', (req, res, next) => {
-  res.status(200).send('hey world hey')
-});
-
-app.get('/bad', (req, res, next) => {
-  next('this route is no bueno');
-});
-
-app.use('*', notFound);
-
-app.use(errorHandler);
-
-app.listen(PORT, () => console.log(`listening on port ${PORT}`));
---
+++ src/app.js
@@ -0,0 +1,29 @@
+'use strict';
+
+const express = require('express');
+const notFound = require('./src/err-handlers/404');
+const errorHandler = require('./src/err-handlers/500')
+const PORT = process.env.PORT || 3002
+
+console.log('helloooo')
+
+const app = express();
+
+app.get('/', (req, res, next) => {
+  res.status(200).send('hey world hey')
+});
+
+app.get('/bad', (req, res, next) => {
+  next('this route is no bueno');
+});
+
+app.use('*', notFound);
+
+app.use(errorHandler);
+
+function start(){
+  app.listen(PORT, () => console.log(`listening on port ${PORT}`));  
+}
+
+module.exports = { app, start };
+
